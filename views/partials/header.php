ff8b9102e26832151d3cecfef70dca414136b2bf
annnnabelle <aj.marcotte@icloud.com>
2025-03-13 12:31:53 -0400

    inscription/connexion

--- views/partials/header.php
+++ views/partials/header.php
@@ -11,7 +11,7 @@
 <header class="head">
     <h2>Knapsack</h2>
     <div class="links">
-        <a href="/inscription" class="inscription">Inscription</a>
-        <a href="/connexion" class="connexion">Connexion</a>
+        <a href="/inscription">Inscription</a>
+        <a href="/connexion">Connexion</a>
     </div>
 </header>
