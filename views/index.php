10fe10821ad0a68fcbd4e654ba443142da58bad5
Etienne0792 <larochelle.etienne@gmail.com>
2025-03-13 10:42:39 -0400

    filterCorrection

--- views/index.php
+++ views/index.php
@@ -83,29 +83,188 @@
             'weight' => 20,
             'price' => 10
         ],
-        
+        [
+            'qty' => 20,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 20,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
+        [
+            'qty' => 2,
+            'img' => '/public/img/FishingRod',
+            'title' => 'TITRE',
+            'type' => "Arme",
+            'weight' => 20,
+            'price' => 10
+        ],
     ];
 ?>
 
 <main>
     <div class="filter">
-        <div>
+        <div class="filterField">
             <input type="checkbox" name="armor" checked>
             <label for="armor">Armure</label>
         </div>
-        <div>
+        <div class="filterField">
             <input type="checkbox" name="weapon" checked>
             <label for="weapon">Armes</label>
         </div>
-        <div>
+        <div class="filterField">
             <input type="checkbox" name="ammo" checked>
             <label for="ammo">Munitions</label>
         </div>
-        <div>
+        <div class="filterField">
             <input type="checkbox" name="food" checked>
             <label for="food">Nourritures</label>
         </div>
-        <div>
+        <div class="filterField">
             <input type="checkbox" name="heal" checked>
             <label for="heal">Médicaments</label>
         </div>
