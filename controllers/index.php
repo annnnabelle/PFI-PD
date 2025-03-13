ed4d6fce79324aa0e008f080f72c5b6fa865427b
annnnabelle <aj.marcotte@icloud.com>
2025-03-13 10:53:01 -0400

    indexModif

--- controllers/index.php
+++ controllers/index.php
@@ -1,3 +1,248 @@
 <?php
 
+$testData = [
+    [
+        'qty' => 20,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 20,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 20,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+    [
+        'qty' => 2,
+        'img' => '/public/img/FishingRod',
+        'title' => 'TITRE',
+        'type' => "Arme",
+        'weight' => 20,
+        'price' => 10
+    ],
+];
+
+
+
 require "views/index.php";
