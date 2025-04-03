5e00cb5b99810a98feb3a5792a04f48cb4c2831f
annnnabelle <aj.marcotte@icloud.com>
2025-04-03 14:11:50 -0400

    Enigme

--- views/enigme.php
+++ views/enigme.php
@@ -2,8 +2,17 @@
 require 'partials/header.php';
 ?>
 
-<main>
+<main class="enigme-background">
 
+    <div class="enigme-bubble">
+
+    </div>
+    <div class="img-enigme">
+        <img src="public/img/catfish.png">
+        <img src="public/img/flounder.png">
+        <img src="public/img/Pufferfish.png">
+        <img src="public/img/squid.png">
+    </div>
 </main>
 
 <?php require 'partials/footer.php'; ?>
