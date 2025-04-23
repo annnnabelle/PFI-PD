b3b0a9ce6c1867a3a655e79b35b35243ae0ba76f
Etienne0792 <larochelle.etienne@gmail.com>
2025-04-23 08:28:44 -0400

    fix confirm

--- controllers/confirm.php
+++ controllers/confirm.php
@@ -12,18 +12,19 @@ if(!isset($_SESSION['info'])) {
 $info = $_SESSION['info'];
 $returnTo=$info['from'];
 
+
 if(isset($_POST['back'])) {
     $_SESSION['returnTo'] = $_SESSION['info']['from'];
+    unset($_SESSION['info']);
     header('Location: ' . $returnTo);
     exit;
 }
 if($info['confirm'] && isset($_POST['confirm'])) {
     $_SESSION['returnTo'] = $_SESSION['info']['from'];
     $_SESSION[$info['return']] = True;
+    unset($_SESSION['info']);
     header('Location: ' . $returnTo);
     exit;
 }
-
-unset($_SESSION['info']);
 
 require "views/confirm.php";
