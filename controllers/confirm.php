b2dacacc209d64758384d0b32b824af5b7516ffc
Etienne0792 <larochelle.etienne@gmail.com>
2025-04-23 07:15:34 -0400

    fix back error

--- controllers/confirm.php
+++ controllers/confirm.php
@@ -4,20 +4,26 @@ require "src/database.php";
 
 session_start();
 
+if(!isset($_SESSION['info'])) {
+    header('Location:' . $_SESSION['returnTo']);
+    exit;
+}
+
 $info = $_SESSION['info'];
 $returnTo=$info['from'];
 
-
 if(isset($_POST['back'])) {
-    unset($_SESSION['info']);
+    $_SESSION['returnTo'] = $_SESSION['info']['from'];
     header('Location: ' . $returnTo);
     exit;
 }
 if($info['confirm'] && isset($_POST['confirm'])) {
+    $_SESSION['returnTo'] = $_SESSION['info']['from'];
     $_SESSION[$info['return']] = True;
-    unset($_SESSION['info']);
     header('Location: ' . $returnTo);
     exit;
 }
+
+unset($_SESSION['info']);
 
 require "views/confirm.php";
