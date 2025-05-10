0516d0328168519ae0790efaf369aeb9da153872
annnnabelle <aj.marcotte@icloud.com>
2025-05-09 20:36:10 -0400

    enigme

--- views/enigme-corriger.php
+++ views/enigme-corriger.php
@@ -4,8 +4,15 @@ require 'partials/header.php';
 
 <main class="enigme-background">
     <div class="enigme-bubble">
-        
+
         <div class="enigme-bubble-header">
+            <?php if (isset($_SESSION['vie_message'])): ?>
+                <p>
+                    <?php echo $_SESSION['vie_message']; ?>
+                </p>
+                <?php unset($_SESSION['vie_message']); ?>
+            <?php endif; ?>
+
             <?php if (isset($_SESSION['answer_feedback'])): ?>
                 <p class="<?php echo $_SESSION['answer_feedback'] === 'Correct!' ? 'correct-text' : 'incorrect-text'; ?>">
                     <?php echo $_SESSION['answer_feedback']; ?>
@@ -19,20 +26,27 @@ require 'partials/header.php';
     </div>
 
     <div class="img-enigme">
-        <div>
-            <a href="/enigme" value="retour">
-                <img src="public/img/catfish.png" alt="catfish">
-                <p>Retour</p>
-            </a>
-        </div>
-        <div>
-            <a href="/enigme-questions?difficulte=<?php echo $_SESSION['current_difficulte'] ?? ''; ?>"
-                value="Continuer">
+    <div>
+        <a href="/enigme" value="retour">
+            <img src="public/img/catfish.png" alt="catfish">
+            <p>Retour</p>
+        </a>
+    </div>
+    <div>
+        <?php if (!empty($_SESSION['plus_de_vie'])): ?>
+            <div style="opacity: 0.5; pointer-events: none;">
+                <img src="public/img/flounder.png" alt="flounder">
+                <p>Continuer</p>
+            </div>
+        <?php else: ?>
+            <a href="/enigme-questions?difficulte=<?php echo $_SESSION['current_difficulte'] ?? ''; ?>" value="Continuer">
                 <img src="public/img/flounder.png" alt="flounder">
                 <p>Continuer</p>
             </a>
-        </div>
+        <?php endif; ?>
     </div>
+</div>
+
 </main>
 
 <?php require 'partials/footer.php'; ?>
